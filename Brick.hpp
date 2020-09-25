ad5bcb977fa29560a2b6bbb0055efc720581dc8d
Pawellos <pablox377@gmail.com>
2020-09-25 23:21:42 +0200

    Bricks placement

--- Brick.hpp
+++ Brick.hpp
@@ -3,12 +3,19 @@
 #include <SFML/Graphics.hpp>
 #include "Entity.hpp"
 
+constexpr float blockWidth{83.f};
+constexpr float blockHeight{30.f};
+constexpr int blocksCol{15};
+constexpr int blocksRow{5};
+
 class Brick : public Entity {
-private:
-    Brick();
+public:
+    Brick(float pos_x, float pos_y);
     void update() override;
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 
 private:
     sf::RectangleShape brick_;
+    sf::Vector2f position_{};
+    sf::Vector2f size_{80.f, 30.f};
 };
--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
+#include "Brick.hpp"
 #include "Game.hpp"
 #include "Paddle.hpp"
 
 int main() {
     auto paddle = std::make_shared<Paddle>();
+
+    std::vector<std::shared_ptr<Brick>> bricks;
+
+    for (int col = 0; col < blocksCol; col++) {
+        for (int row = 0; row < blocksRow; row++) {
+            bricks.push_back(std::make_shared<Brick>((col * (blockWidth + 2)) + 3, (row * (blockHeight + 3)) + 100));
+        }
+    }
+    auto brick = std::make_shared<Brick>(0, 0);
     Game game;
     game.addDrawObject(paddle);
+    for (auto brick : bricks) {
+        game.addDrawObject(brick);
+    }
     game.run();
 }
