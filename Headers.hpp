d7925061ebee8db6025f9e99d0f685a8a7a05299
Kasper Biegun <kasper.biegun95@gmail.com>
2020-09-25 22:24:14 +0200

    Ball movement + wall detection

--- Headers.hpp
+++ Headers.hpp
@@ -4,4 +4,7 @@
 constexpr size_t windowWidth = 1280;
 constexpr size_t windowHeight = 720;
 
+constexpr float ballSpeed{8.f};
+constexpr int ballRadius{10};
+
 #include "Entity.hpp"
--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
+#include "Ball.hpp"
 #include "Game.hpp"
 #include "Paddle.hpp"
 
 int main() {
     auto paddle = std::make_shared<Paddle>();
+    auto ball = std::make_shared<Ball>();
     Game game;
     game.addDrawObject(paddle);
+    game.addDrawObject(ball);
     game.run();
 }
