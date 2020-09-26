ff195388896cb956ef9bb7d57d7fa6856abb08ed
Kasper Biegun <kasper.biegun95@gmail.com>
2020-09-26 19:05:15 +0200

    Added game over sound

--- GameOver.cpp
+++ GameOver.cpp
@@ -5,10 +5,14 @@ GameOver::GameOver() {
     gameOver_.setTexture(&gameOverTexture_);
     gameOver_.setSize(sf::Vector2f(gameOverX, gameOverY));
     gameOver_.setPosition(sf::Vector2f(windowWidth / 2 - gameOverX / 2, windowHeight + gameOverY));
+
+    gameOverBuffer_.loadFromFile("../assets/sounds/gameOverMusic.wav");
+    gameOverSound_.setBuffer(gameOverBuffer_);
 }
 
 void GameOver::update() {
     if (gameOver_.getPosition().y >= windowHeight / 2) {
+        gameOverSound_.play();
         gameOver_.move(gameOverSpeed_);
     }
 }
